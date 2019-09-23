06e9350ac00f80bdf9eb5c2b9fb67f35a80bb4bf
novotny <novotny@porg.cz>
2019-09-23 17:34:37 +0200

    Server na webu

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ http.createServer((req, res) => {
     if (req.url == "/") {
         x++;
     }
-    res.writeHead(200, {"ContentType" : "text/html"});
-    res.end('<html lang="cs"><head><meta charset="UTF8"></head><body>Součet je ' + x +'</body></html>');
+    if (req.url == "/jinastranka") {
+        res.writeHead(200, {"ContentType" : "text/html"});
+        res.end('<html lang="cs"><head><meta charset="UTF8"></head><body>Našel jsi tajnou stránku</body></html>');
+    }  else if (req.url == "/json") {
+        res.writeHead(200, {"ContentType" : "application/json"});
+        let obj = {};
+        obj.jmeno = "Borec";
+        obj.prijmeni = "Je Dan";
+        res.end(JSON.stringify(obj));
+    }  else {
+        res.writeHead(200, {"ContentType" : "text/html"});
+        res.end('<html lang="cs"><head><meta charset="UTF8"></head><body>Součet je ' + x +'</body></html>');
+    }
 }).listen(8888);
